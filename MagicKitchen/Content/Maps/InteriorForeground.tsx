063de5c61e3d377c31a855273840b053d059c136
Tucker Higgins <thiggin6@lion.lmu.edu>
2022-04-02 12:40:34 -0700

    interior changes

--- MagicKitchen/Content/Maps/InteriorForeground.tsx
+++ MagicKitchen/Content/Maps/InteriorForeground.tsx
@@ -1,6 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.0" name="InteriorForeground" tilewidth="16" tileheight="16" tilecount="4900" columns="70">
  <image source="InteriorForeground.png" width="1120" height="1120"/>
+ <tile id="1273">
+  <properties>
+   <property name="newHitBox" value="0,-28,16,52"/>
+   <property name="newSource" value="0,-32,16,48"/>
+  </properties>
+ </tile>
+ <tile id="1412">
+  <properties>
+   <property name="newHitBox" value="0,-8,16,24"/>
+   <property name="newSource" value="0,-16,16,32"/>
+  </properties>
+ </tile>
+ <tile id="1413">
+  <properties>
+   <property name="newHitBox" value="0,-8,16,24"/>
+   <property name="newSource" value="0,-16,16,32"/>
+  </properties>
+ </tile>
+ <tile id="1414">
+  <properties>
+   <property name="newHitBox" value="0,-8,16,24"/>
+   <property name="newSource" value="0,-16,16,32"/>
+  </properties>
+ </tile>
  <tile id="2812">
   <properties>
    <property name="newSource" value="0,-48,16,64"/>
