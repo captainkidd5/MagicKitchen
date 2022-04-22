a89f1b5d1c1def4187f617d2418a11c7c4e8188a
Tucker Higgins <thiggin6@lion.lmu.edu>
2022-04-22 14:11:00 -0700

    more script progress

--- DataModels/ScriptedEventStuff/ScriptAction.cs
+++ DataModels/ScriptedEventStuff/ScriptAction.cs
@@ -4,13 +4,23 @@ using System.Text;
 
 namespace DataModels.ScriptedEventStuff
 {
+    public enum ScriptActionType
+    {
+        None = 0,
+        Move = 1,
+        Pause = 2,
+    }
     public class ScriptAction
     {
+        public ScriptActionType Type { get; set; }
         public int TileX { get; set; }
         public int TileY { get; set; }
 
         public string ZoneStart { get; set; }
         public string ZoneEnd { get; set; }
+
+
+        public int PauseForSeconds { get; set; }
 
         public int Speed { get; set; }
     }
--- EntityEngine/Classes/BehaviourStuff/ScriptBehaviour.cs
+++ EntityEngine/Classes/BehaviourStuff/ScriptBehaviour.cs
@@ -23,15 +23,15 @@ namespace EntityEngine.Classes.BehaviourStuff
         private SubScript _currentScript;
         private ScriptAction _currentAction;
         private int _currentActionStep = 0;
-        private bool _active;
+        private bool _started;
         public ScriptBehaviour(Entity entity, StatusIcon statusIcon, Navigator navigator,
             TileManager tileManager, float? timerFrequency) :
             base(entity, statusIcon, navigator, tileManager, timerFrequency)
         {
             _tileManager = tileManager;
-            
+
         }
-       
+
 
 
         public void InjectSubscript(SubScript subscript)
@@ -45,41 +45,75 @@ namespace EntityEngine.Classes.BehaviourStuff
         public override void Update(GameTime gameTime, ref Vector2 velocity)
         {
             base.Update(gameTime, ref velocity);
-
-            if (SimpleTimer.Run(gameTime))
+            if (_currentActionStep >= _currentScript.ScriptActions.Count)
             {
-                if(_currentActionStep > _currentScript.ScriptActions.Count )
-                {
-                    //Complete
-                    return;
-                }
-       
+                //Complete
+                return;
             }
-            if (Navigator.HasActivePath)
+            if (!_started)
             {
-                if(Navigator.FollowPath(gameTime, Entity.Position, ref velocity))
-                {
-                    if (_currentActionStep >= _currentScript.ScriptActions.Count - 1)
-                    {
-                        //Complete
-                        return;
-                    }
-                    else
-                    {
-                        _currentActionStep++;
-
-                        _currentAction = _currentScript.ScriptActions[_currentActionStep];
-                    }
-                }
-
-            }
-            else
-            {
-                Entity.Halt();
                 GetNextStep();
+                _started = true;
             }
+            FollowStep(gameTime, ref velocity);
 
 
+        }
+        private void FollowStep(GameTime gameTime, ref Vector2 velocity)
+        {
+            switch (_currentAction.Type)
+            {
+                case ScriptActionType.None:
+                    break;
+                case ScriptActionType.Move:
+                    if (Navigator.HasActivePath)
+                    {
+                        if (Navigator.FollowPath(gameTime, Entity.Position, ref velocity))
+                        {
+
+                            CompleteStep();
+                            GetNextStep();
+
+                        }
+
+                    }
+                    break;
+                case ScriptActionType.Pause:
+                    if (SimpleTimer.Run(gameTime))
+                    {
+                        CompleteStep();
+
+                        GetNextStep();
+
+                    }
+                    break;
+            }
+        }
+        /// <summary>
+        /// Call to increment current step
+        /// </summary>
+        private void CompleteStep()
+        {
+            _currentActionStep++;
+
+            if(_currentActionStep < _currentScript.ScriptActions.Count)
+                _currentAction = _currentScript.ScriptActions[_currentActionStep];
+            Entity.Halt();
+        }
+
+        private void GetNextStep()
+        {
+            switch (_currentAction.Type)
+            {
+                case ScriptActionType.None:
+                    break;
+                case ScriptActionType.Move:
+                    GetStepPath();
+                    break;
+                case ScriptActionType.Pause:
+                    SimpleTimer.SetNewTargetTime(_currentAction.PauseForSeconds);
+                    break;
+            }
         }
 
         public override void DrawDebug(SpriteBatch spriteBatch)
@@ -89,35 +123,35 @@ namespace EntityEngine.Classes.BehaviourStuff
 
         }
 
-        private void GetNextStep()
+        private void GetStepPath()
         {
- 
-                Vector2 targetpos = Vector2.Zero;
-                if (!string.IsNullOrEmpty(_currentAction.ZoneEnd))
-                    {
-                    var zones = TileManager.GetZones(_currentAction.ZoneEnd.Split(',')[0]);
 
-                    if (zones != null)
-                    {
-                        var startZone = zones.FirstOrDefault(x => x.Value == _currentAction.ZoneStart.Split(',')[1]);
-                        Entity.Move(startZone.Position);
-                        var endZone = zones.FirstOrDefault(x => x.Value == _currentAction.ZoneEnd.Split(',')[1]);
-                        if (endZone != null)
-                            targetpos = endZone.Position;
+            Vector2 targetpos = Vector2.Zero;
+            if (!string.IsNullOrEmpty(_currentAction.ZoneEnd))
+            {
+                var zones = TileManager.GetZones(_currentAction.ZoneEnd.Split(',')[0]);
 
-                    }
-
-                }
-                else
+                if (zones != null)
                 {
-                    targetpos = Vector2Helper.GetWorldPositionFromTileIndex(
-                    _currentAction.TileX, _currentAction.TileY);
+                    var startZone = zones.FirstOrDefault(x => x.Value == _currentAction.ZoneStart.Split(',')[1]);
+                    Entity.Move(startZone.Position);
+                    var endZone = zones.FirstOrDefault(x => x.Value == _currentAction.ZoneEnd.Split(',')[1]);
+                    if (endZone != null)
+                        targetpos = endZone.Position;
+
                 }
 
-                base.GetPath(targetpos, Entity.CurrentStageName);
-               // CommandConsole.Append($"{Entity.Name} current location : {Entity.CurrentStageName}");
+            }
+            else
+            {
+                targetpos = Vector2Helper.GetWorldPositionFromTileIndex(
+                _currentAction.TileX, _currentAction.TileY);
+            }
 
-     
+            base.GetPath(targetpos, Entity.CurrentStageName);
+            // CommandConsole.Append($"{Entity.Name} current location : {Entity.CurrentStageName}");
+
+
         }
 
         public override void OnCollides(Fixture fixtureA, Fixture fixtureB, Contact contact)
