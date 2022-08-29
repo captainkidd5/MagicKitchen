cb6c7401ccbe6411666fc0c95f7dd0f5f249bbbd
Tucker Higgins <thiggin6@lion.lmu.edu>
2022-08-29 12:51:43 -0700

    add pause duration for individual characters

--- DataModels/JsonConverters/LetterData.cs
+++ DataModels/JsonConverters/LetterData.cs
@@ -11,7 +11,14 @@ namespace DataModels.JsonConverters
 
         public int Width { get; set; }
         public int Height { get; set; }
+        /// <summary>
+        /// Default is 8, otherwise gap is given. Usually used for wider characters which need more breathing room
+        /// </summary>
         public int OverrideGap { get; set; }
+        /// <summary>
+        /// How long the text builder will pause for after each character. Default will be provided if none given. Result will divide this by 100.
+        /// </summary>
+        public float PauseDuration { get; set; }
         public Dictionary<Char, int> KerningPairs { get; set; }
     }
 }
--- TextEngine/Classes/TextBuilder.cs
+++ TextEngine/Classes/TextBuilder.cs
@@ -18,7 +18,7 @@ namespace TextEngine.Classes
         public bool IsComplete => _currentText.ToString() == _desiredString;
         public void ClearCurrent()
         {
-        _currentText.Clear();
+            _currentText.Clear();
             _currentIndex = 0;
         }
 
@@ -29,7 +29,7 @@ namespace TextEngine.Classes
         public TextBuilder(String value, Vector2 position, float? lineXStart, float? lineLimit, float layerDepth, ImageFont imageFont = null, FontType? fontType = null, Color? color = null, float? scale = null)
         {
             _desiredString = value;
-            _currentText = TextFactory.CreateUIText(string.Empty, position, lineXStart, lineLimit, layerDepth,imageFont, fontType,color,scale);
+            _currentText = TextFactory.CreateUIText(string.Empty, position, lineXStart, lineLimit, layerDepth, imageFont, fontType, color, scale);
             _textTimer = new SimpleTimer(_typeTime);
         }
 
@@ -42,25 +42,42 @@ namespace TextEngine.Classes
         }
         public bool Update(GameTime gameTime, Vector2 position, float lineLimit)
         {
-        
-            if (!IsComplete && _textTimer.Run(gameTime) )
+
+            if (!IsComplete && _textTimer.Run(gameTime))
             {
-                if (_desiredString.ElementAt(_currentIndex) == ',')
-                    _textTimer.SetNewTargetTime(_pauseTime);
-                else
-                    _textTimer.SetNewTargetTime(_typeTime);
+                float pauseDuration = GetCharacterPauseDuration();
+
+                _textTimer.SetNewTargetTime(pauseDuration);
+
                 _currentText.Append(_desiredString.ElementAt(_currentIndex));
                 _currentIndex++;
-            
+
 
             }
             _currentText.Update(position, null, lineLimit);
             return IsComplete;
         }
 
+        
+        /// <summary>
+        /// Each character may specify the duration to be paused for. Defaults to field if none given
+        /// </summary>
+        /// <returns></returns>
+        private float GetCharacterPauseDuration()
+        {
+
+            char c = _desiredString.ElementAt(_currentIndex);
+            if (c == ' ')
+                return _typeTime;
+            float pauseDuration = LanguageManager.CurrentLanguage.CharPoints[c].PauseDuration == 0 ?
+                _typeTime : LanguageManager.CurrentLanguage.CharPoints[c].PauseDuration / 100;
+
+            return pauseDuration;
+        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
-            _currentText.Draw(spriteBatch,.99f);
+            _currentText.Draw(spriteBatch, .99f);
         }
 
         public void ForceComplete()
